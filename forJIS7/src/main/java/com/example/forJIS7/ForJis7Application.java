b6a28cbb03b927e5aa2a169d960a976448532096
Александр <66840483+Sarmat92@users.noreply.github.com>
2021-04-04 17:29:43 +0300

    Add files via upload

---
+++ forJIS7/src/main/java/com/example/forJIS7/ForJis7Application.java
@@ -0,0 +1,13 @@
+package com.example.forJIS7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForJis7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForJis7Application.class, args);
+	}
+
+}
---
+++ forJIS7/src/test/java/com/example/forJIS7/ForJis7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forJIS7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForJis7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
