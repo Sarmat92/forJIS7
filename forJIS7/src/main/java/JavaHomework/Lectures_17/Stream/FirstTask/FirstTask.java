42e6d5153b48b99b468bc7f010eb7ef653964cac
Alexandr Kas <joedassin2015@gmail.com>
2021-05-25 19:19:30 +0300

    random number Stream_API

---
+++ forJIS7/src/main/java/JavaHomework/Lectures_17/Stream/FirstTask/FirstTask.java
@@ -0,0 +1,21 @@
+package JavaHomework.Lectures_17.Stream.FirstTask;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class FirstTask {
+
+    public static Set<Integer>randomNumber(int count){
+        return new Random()
+                .ints(0,100)
+                .distinct()
+                .limit(count)
+                .boxed()
+                .collect(Collectors.toSet());
+    }
+    public static void main(String[] args) {
+
+        System.out.println(randomNumber(5));
+    }
+}
