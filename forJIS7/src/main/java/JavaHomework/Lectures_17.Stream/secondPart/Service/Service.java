e958dab1c7fe0189d27ecef01956fe5973111c27
Alexandr Kas <joedassin2015@gmail.com>
2021-06-07 20:06:14 +0300

    add checkUserStatus, addUserList

--- forJIS7/src/main/java/JavaHomework/Lectures_17.Stream/secondPart/Service/Service.java
+++ forJIS7/src/main/java/JavaHomework/Lectures_17.Stream/secondPart/Service/Service.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface Service {
 
     List<User> checkUserStatus();
+
+    void addUserList();
 }
--- forJIS7/src/main/java/JavaHomework/Lectures_17.Stream/secondPart/Service/UserService.java
+++ forJIS7/src/main/java/JavaHomework/Lectures_17.Stream/secondPart/Service/UserService.java
@@ -15,7 +15,19 @@ public class UserService implements Service {
         List<User> userStatus = userList.stream()
                 .filter(it -> it.getUserStatus().equals(UserStatus.GUEST))
                 .collect(Collectors.toList());
-
         return userStatus;
+    }
+
+    @Override
+    public void addUserList() {
+
+        userList.add(new User(1, "Boris", "Burkin", "Burboria", 45,
+                "bukinboris1975@gmail.com", 12345, UserStatus.GUEST, true));
+
+        userList.add(new User(2, "Dima", "Rogov", "Rogatka", 18,
+                "dimarog@yandex.ru", 123, UserStatus.USER, true));
+
+        userList.add(new User(3, "Sasha", "Gromov", "SashaGrom", 25,
+                "gromov@mail.ru", 852, UserStatus.VIP_USER, false));
     }
 }
