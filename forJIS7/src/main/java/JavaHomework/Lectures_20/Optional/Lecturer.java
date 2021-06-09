67591b7aef92996d7e10beda4cc14a1c9048fca2
Alexandr Kas <joedassin2015@gmail.com>
2021-06-09 21:27:56 +0300

    create project

---
+++ forJIS7/src/main/java/JavaHomework/Lectures_20/Optional/Lecturer.java
@@ -0,0 +1,16 @@
+package JavaHomework.Lectures_20.Optional;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Lecturer {
+
+    private String firstName;
+    private String lastName;
+    private Integer age;
+}
---
+++ forJIS7/src/main/java/JavaHomework/Lectures_20/Optional/Student.java
@@ -0,0 +1,16 @@
+package JavaHomework.Lectures_20.Optional;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private Integer age;
+}
