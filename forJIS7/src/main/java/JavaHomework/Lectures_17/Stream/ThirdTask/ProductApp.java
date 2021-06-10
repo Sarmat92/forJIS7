8d35b829c18656bfec055461cfa48df60b71cc82
Alexandr Kas <joedassin2015@gmail.com>
2021-06-10 20:44:15 +0300

    rename method

--- forJIS7/src/main/java/JavaHomework/Lectures_17/Stream/ThirdTask/ProductApp.java
+++ forJIS7/src/main/java/JavaHomework/Lectures_17/Stream/ThirdTask/ProductApp.java
@@ -1,22 +1,15 @@
 package JavaHomework.Lectures_17.Stream.ThirdTask;
 
-import JavaHomework.Lectures_17.Stream.ThirdTask.Model.Product;
 import JavaHomework.Lectures_17.Stream.ThirdTask.ProductService.*;
-
-import static JavaHomework.Lectures_17.Stream.ThirdTask.Repository.ProductRepository.*;
+import JavaHomework.Lectures_17.Stream.ThirdTask.Repository.ProductRepository;
 
 public class ProductApp {
 
     public static void main(String[] args) {
 
-        productList.add(new Product("Milk", 25.99, 45));
-        productList.add(new Product("Coffee", 15.99,0));
-        productList.add(new Product("Tee", 2.99,0));
-        productList.add(new Product("Bread",8.99,50));
-        productList.add(new Product("Beer", 1.99,10));
-        productList.add(new Product("Potato",0.99,5));
+        ProductRepository.initializeProductList();
 
-        var productService = new ProductService();
+        var productService = new ProductServiceImpl();
 
         System.out.println(productService.findProductByDiscount());
         System.out.println(productService.minProductPrice());
--- forJIS7/src/main/java/JavaHomework/Lectures_17/Stream/ThirdTask/Repository/ProductRepository.java
+++ forJIS7/src/main/java/JavaHomework/Lectures_17/Stream/ThirdTask/Repository/ProductRepository.java
@@ -9,4 +9,12 @@ public class ProductRepository {
 
     public static List<Product> productList = new ArrayList<>();
 
+    public static void initializeProductList() {
+        productList.add(new Product("Milk", 25.99, 45));
+        productList.add(new Product("Coffee", 15.99, 0));
+        productList.add(new Product("Tee", 2.99, 0));
+        productList.add(new Product("Bread", 8.99, 50));
+        productList.add(new Product("Beer", 1.99, 10));
+        productList.add(new Product("Potato", 0.99, 5));
+    }
 }
