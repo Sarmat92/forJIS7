0dde279f9a8ae8b262a77b2370d0623d55fcbd44
Alexandr Kas <joedassin2015@gmail.com>
2021-06-20 16:12:08 +0300

    initialize productList with List.of()

--- forJIS7/src/main/java/JavaHomework/Lectures_17/Stream/ThirdTask/Repository/ProductRepository.java
+++ forJIS7/src/main/java/JavaHomework/Lectures_17/Stream/ThirdTask/Repository/ProductRepository.java
@@ -10,11 +10,17 @@ public class ProductRepository {
     public static List<Product> productList = new ArrayList<>();
 
     public static void initializeProductList() {
-        productList.add(new Product("Milk", 25.99, 45));
-        productList.add(new Product("Coffee", 15.99, 0));
-        productList.add(new Product("Tee", 2.99, 0));
-        productList.add(new Product("Bread", 8.99, 50));
-        productList.add(new Product("Beer", 1.99, 10));
-        productList.add(new Product("Potato", 0.99, 5));
+
+        productList = List.of(
+                new Product("Milk",25.99,45),
+                new Product("Coffee", 15.99, 0),
+                new Product("Tee", 2.99, 0),
+                new Product("Bread", 8.99, 50),
+                new Product("Beer", 1.99, 10),
+                new Product("Potato", 0.99, 5)
+        );
+
+
+
     }
 }
